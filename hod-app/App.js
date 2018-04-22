362bbba394c567bf7ad6a84680c32c817cd50814
Santiago Pravisani <santiagopravisani@gmail.com>
2018-04-21 23:58:00 -0300

    Initial screen

--- hod-app/App.js
+++ hod-app/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    email: ''
+  }
+
+  handleEmail = (text) => this.setState({email: text })
+
+  handlePress = () => {}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>VIVAN LOS MEMES!!!</Text>
+        <Text>House of Debts</Text>
+        <TextInput placeholder="Ingrese su email" onChangeText={this.handleEmail}/>
+        <Button title="Ingresar" onPress={this.handlePress}/>
       </View>
     );
   }
