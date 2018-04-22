9060b6506e76745ba613a39804fb5816bd95de71
Santiago Pravisani <santiagopravisani@gmail.com>
2018-04-22 01:44:45 -0300

    Moving between home and other screens

--- hod-app/components/HomeScreen.js
+++ hod-app/components/HomeScreen.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
 
 export default class HomeScreen extends React.Component {
 
-  render(){
+  _navigateTo(screen, body){
+    return this.props.navigation.navigate(screen, body);
+  }
 
+  handleDepositPress = () => this._navigateTo('Deposit', {email: this.state.email});
+
+  handleMyDebtsPress = () => this._navigateTo('Balance', {email: this.state.email});
+
+  componentWillMount() {
     /* Read the params from the Login Screen state */
     const { params } = this.props.navigation.state;
     const email = params ? params.email : null;
+    this.setState({email: email});
 
+  }
+
+  render(){
     return (
       <View style={styles.container}>
         <Text> Main menu !!</Text>
-        <Text> Welcolme {email}</Text>
+        <Text> Welcolme {this.state.email}</Text>
+        <Button title="Deposit $$" onPress={ this.handleDepositPress }/>
+        <Button title="My Debts" onPress={ this.handleMyDebtsPress }/>
       </View>
       );
   }
