9760586167f68e3b8053d7256af0e2f2a2138644
Cristian <cgley45@gmail.com>
2018-04-22 04:12:46 -0300

    Styles add

--- hod-app/components/DepositScreen.js
+++ hod-app/components/DepositScreen.js
@@ -1,14 +1,39 @@
 import React from 'react';
 
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 
 export default class DepositScreen extends React.Component {
 
+state = {
+    email: ''
+  }
+
+handleDepositPress = () => { /*
+  fetch('https://mywebsite.com/endpoint/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+    secondParam: 'yourOtherValue',
+  }),
+});    */
+this.props.navigation.navigate('Home', {email: this.state.email})
+}
   render(){
 
     return (
       <View style={styles.container}>
         <Text> Deposit view</Text>
+        <Text> Ingrese los siguientes campos </Text>
+        <TextInput style={styles.input} placeholder="Ingrese monto" />
+        <TextInput style={styles.input} placeholder="Ingrese casa" />
+        <TextInput  style={styles.input} placeholder="Ingrese descripcion" />
+        <Button title="Depositar" onPress={this.handleDepositPress} />
+        
+        
       </View>
       );
   }
@@ -20,5 +45,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  input:{
+    padding: 10,
+    margin: 10, 
+    width:250,
   },
 });
